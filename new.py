3169f6778e0b2ec0aea1b16d8b9c4a54ce39722d
Carolyn Kuebler <ckuebler001@student.franciscan.edu>
2024-12-02 14:09:36 -0500

    working

--- new.py
+++ new.py
@@ -29,17 +29,17 @@ f1.pack(fill=tk.BOTH, expand=True)
 f1.rowconfigure([0,1], minsize=20, weight=1)
 f1.columnconfigure([0,1], minsize=20, weight=1)
 
-b11=tk.Button(master=f1, text="ultimate frisbee", height=10)
-b11.grid(row=0, column=0, sticky="nsew")
+b1=tk.Button(master=f1, text="ultimate frisbee", height=10)
+b1.grid(row=0, column=0, sticky="nsew")
 
-b12=tk.Button(master=f1, text="volleyball")
-b12.grid(row=0, column=1, sticky="nsew")
+b2=tk.Button(master=f1, text="volleyball")
+b2.grid(row=0, column=1, sticky="nsew")
 
-b13=tk.Button(master=f1, text="soccer", height=10)
-b13.grid(row=1, column=0, sticky="nsew")
+b3=tk.Button(master=f1, text="soccer", height=10)
+b3.grid(row=1, column=0, sticky="nsew")
 
-b14=tk.Button(master=f1, text="I don't play sports")
-b14.grid(row=1, column=1, sticky="nsew")
+b4=tk.Button(master=f1, text="I don't play sports")
+b4.grid(row=1, column=1, sticky="nsew")
 
 
 
@@ -53,262 +53,201 @@ f2.pack(fill=tk.BOTH, expand=True)
 f2.rowconfigure([0,1], minsize=20, weight=1)
 f2.columnconfigure([0,1], minsize=20, weight=1)
 
-b21=tk.Button(master=f1, text="AMDG", height=10)
-b21.grid(row=0, column=0, sticky="nsew")
-
-b22=tk.Button(master=f1, text="Fishers")
-b22.grid(row=0, column=1, sticky="nsew")
-
-b23=tk.Button(master=f1, text="DDM", height=10)
-b23.grid(row=1, column=0, sticky="nsew")
-
-b24=tk.Button(master=f1, text="Stella")
-b24.grid(row=1, column=1, sticky="nsew")
-
-
-
-
-
-label1=tk.Label(master=window,text="Choose a household", width=50, height=10)
-label1.pack()
-
-f1=tk.Frame(master=window)
-f1.pack(fill=tk.BOTH, expand=True)
-
-f1.rowconfigure([0,1], minsize=20, weight=1)
-f1.columnconfigure([0,1], minsize=20, weight=1)
-
-b1=tk.Button(master=f1, text="Stella", height=10)
+b1=tk.Button(master=f2, text="AMDG", height=10)
 b1.grid(row=0, column=0, sticky="nsew")
 
-b2=tk.Button(master=f1, text="Rosa Mystica")
+b2=tk.Button(master=f2, text="Fishers")
 b2.grid(row=0, column=1, sticky="nsew")
 
-b3=tk.Button(master=f1, text="DOZ", height=10)
+b3=tk.Button(master=f2, text="DDM", height=10)
 b3.grid(row=1, column=0, sticky="nsew")
 
-b4=tk.Button(master=f1, text="DDM")
+b4=tk.Button(master=f2, text="Stella")
 b4.grid(row=1, column=1, sticky="nsew")
 
 
 
 
-label1=tk.Label(master=window,text="Who is your favorite professor", width=50, height=10)
+label1=tk.Label(master=content_frame,text="Who is your favorite professor?", width=50, height=10)
 label1.pack()
 
-f1=tk.Frame(master=window)
-f1.pack(fill=tk.BOTH, expand=True)
+f3=tk.Frame(master=content_frame)
+f3.pack(fill=tk.BOTH, expand=True)
 
-f1.rowconfigure([0,1], minsize=20, weight=1)
-f1.columnconfigure([0,1], minsize=20, weight=1)
+f3.rowconfigure([0,1], minsize=20, weight=1)
+f3.columnconfigure([0,1], minsize=20, weight=1)
 
-b1=tk.Button(master=f1, text="Scott Hahn", height=10)
+b1=tk.Button(master=f3, text="Scott Hahn", height=10)
 b1.grid(row=0, column=0, sticky="nsew")
 
-b2=tk.Button(master=f1, text="Dr. Reinhard")
+b2=tk.Button(master=f3, text="Dr. Reinhard")
 b2.grid(row=0, column=1, sticky="nsew")
 
-b3=tk.Button(master=f1, text="Dr. Kuebler", height=10)
+b3=tk.Button(master=f3, text="Dr. Kuebler", height=10)
 b3.grid(row=1, column=0, sticky="nsew")
 
-b4=tk.Button(master=f1, text="Dr. Knox")
+b4=tk.Button(master=f3, text="Dr. Knox")
 b4.grid(row=1, column=1, sticky="nsew")
 
 
 
-label1=tk.Label(master=window,text="What's your major?", width=50, height=10)
+label1=tk.Label(master=content_frame,text="What's your major?", width=50, height=10)
 label1.pack()
 
-f1=tk.Frame(master=window)
-f1.pack(fill=tk.BOTH, expand=True)
+f4=tk.Frame(master=content_frame)
+f4.pack(fill=tk.BOTH, expand=True)
 
-f1.rowconfigure([0,1], minsize=20, weight=1)
-f1.columnconfigure([0,1], minsize=20, weight=1)
+f4.rowconfigure([0,1], minsize=20, weight=1)
+f4.columnconfigure([0,1], minsize=20, weight=1)
 
-b1=tk.Button(master=f1, text="TheoCat", height=10)
+b1=tk.Button(master=f4, text="TheoCat", height=10)
 b1.grid(row=0, column=0, sticky="nsew")
 
-b2=tk.Button(master=f1, text="Engineering")
+b2=tk.Button(master=f4, text="Nursing")
 b2.grid(row=0, column=1, sticky="nsew")
 
-b3=tk.Button(master=f1, text="Computer Sci", height=10)
+b3=tk.Button(master=f4, text="Business", height=10)
 b3.grid(row=1, column=0, sticky="nsew")
 
-b4=tk.Button(master=f1, text="Philosophy")
+b4=tk.Button(master=f4, text="Psych")
+b4.grid(row=1, column=1, sticky="nsew")
+
+
+label1=tk.Label(master=content_frame,text="What's your favorite mass time?", width=50, height=10)
+label1.pack()
+
+f6=tk.Frame(master=content_frame)
+f6.pack(fill=tk.BOTH, expand=True)
+
+f6.rowconfigure([0,1], minsize=20, weight=1)
+f6.columnconfigure([0,1], minsize=20, weight=1)
+
+b1=tk.Button(master=f6, text="6:30 am", height=10)
+b1.grid(row=0, column=0, sticky="nsew")
+
+b2=tk.Button(master=f6, text="10:00 am ")
+b2.grid(row=0, column=1, sticky="nsew")
+
+b3=tk.Button(master=f6, text="Noon")
+b3.grid(row=1, column=0, sticky="nsew")
+
+b4=tk.Button(master=f6, text="8 pm")
 b4.grid(row=1, column=1, sticky="nsew")
 
 
 
-label1=tk.Label(master=window,text="What's your major?", width=50, height=10)
+label1=tk.Label(master=content_frame,text="Pick a place to study", width=50, height=10)
 label1.pack()
 
-f1=tk.Frame(master=window)
-f1.pack(fill=tk.BOTH, expand=True)
+f7=tk.Frame(master=content_frame)
+f7.pack(fill=tk.BOTH, expand=True)
 
-f1.rowconfigure([0,1], minsize=20, weight=1)
-f1.columnconfigure([0,1], minsize=20, weight=1)
+f7.rowconfigure([0,1], minsize=20, weight=1)
+f7.columnconfigure([0,1], minsize=20, weight=1)
 
-b1=tk.Button(master=f1, text="TheoCat", height=10)
+b1=tk.Button(master=f7, text="CTT", height=10)
 b1.grid(row=0, column=0, sticky="nsew")
 
-b2=tk.Button(master=f1, text="Nursing")
+b2=tk.Button(master=f7, text="Library")
 b2.grid(row=0, column=1, sticky="nsew")
 
-b3=tk.Button(master=f1, text="Education", height=10)
+b3=tk.Button(master=f7, text="Dorm", height=10)
 b3.grid(row=1, column=0, sticky="nsew")
 
-b4=tk.Button(master=f1, text="Psych")
+b4=tk.Button(master=f7, text="JC")
 b4.grid(row=1, column=1, sticky="nsew")
 
 
 
-label1=tk.Label(master=window,text="What's your favorite mass time?", width=50, height=10)
+label1=tk.Label(master=content_frame,text="Favorite Cafe meal", width=50, height=10)
 label1.pack()
 
-f1=tk.Frame(master=window)
-f1.pack(fill=tk.BOTH, expand=True)
+f8=tk.Frame(master=content_frame)
+f8.pack(fill=tk.BOTH, expand=True)
 
-f1.rowconfigure([0,1], minsize=20, weight=1)
-f1.columnconfigure([0,1], minsize=20, weight=1)
+f8.rowconfigure([0,1], minsize=20, weight=1)
+f8.columnconfigure([0,1], minsize=20, weight=1)
 
-b1=tk.Button(master=f1, text="6:30 am", height=10)
+b1=tk.Button(master=f8, text="Pizza", height=10)
 b1.grid(row=0, column=0, sticky="nsew")
 
-b2=tk.Button(master=f1, text="10:00 am ")
+b2=tk.Button(master=f8, text="Stir Fry")
 b2.grid(row=0, column=1, sticky="nsew")
 
-b3=tk.Button(master=f1, text="Noon")
+b3=tk.Button(master=f8, text="Chicken and Rice", height=10)
 b3.grid(row=1, column=0, sticky="nsew")
 
-b4=tk.Button(master=f1, text="8 pm")
+b4=tk.Button(master=f8, text="Cereal and Ice Cream")
 b4.grid(row=1, column=1, sticky="nsew")
 
 
 
-label1=tk.Label(master=window,text="Pick a place to study", width=50, height=10)
+label1=tk.Label(master=content_frame,text="What do you do in your freetime?", width=50, height=10)
 label1.pack()
 
-f1=tk.Frame(master=window)
-f1.pack(fill=tk.BOTH, expand=True)
+f9=tk.Frame(master=content_frame)
+f9.pack(fill=tk.BOTH, expand=True)
 
-f1.rowconfigure([0,1], minsize=20, weight=1)
-f1.columnconfigure([0,1], minsize=20, weight=1)
+f9.rowconfigure([0,1], minsize=20, weight=1)
+f9.columnconfigure([0,1], minsize=20, weight=1)
 
-b1=tk.Button(master=f1, text="CTT", height=10)
+b1=tk.Button(master=f9, text="Read spiritual boooks outside", height=10)
 b1.grid(row=0, column=0, sticky="nsew")
 
-b2=tk.Button(master=f1, text="Library")
+b2=tk.Button(master=f9, text="Go on long walks with your Frannie significant other")
 b2.grid(row=0, column=1, sticky="nsew")
 
-b3=tk.Button(master=f1, text="Dorm", height=10)
+b3=tk.Button(master=f9, text="Always studying", height=10)
 b3.grid(row=1, column=0, sticky="nsew")
 
-b4=tk.Button(master=f1, text="JC")
+b4=tk.Button(master=f9, text="I'm a JC rat")
 b4.grid(row=1, column=1, sticky="nsew")
 
 
 
-label1=tk.Label(master=window,text="Favorite Cafe meal", width=50, height=10)
+label1=tk.Label(master=content_frame,text="How often do you go to Latin Mass?", width=50, height=10)
 label1.pack()
 
-f1=tk.Frame(master=window)
-f1.pack(fill=tk.BOTH, expand=True)
+f10=tk.Frame(master=content_frame)
+f10.pack(fill=tk.BOTH, expand=True)
 
-f1.rowconfigure([0,1], minsize=20, weight=1)
-f1.columnconfigure([0,1], minsize=20, weight=1)
+f10.rowconfigure([0,1], minsize=20, weight=1)
+f10.columnconfigure([0,1], minsize=20, weight=1)
 
-b1=tk.Button(master=f1, text="Pizza", height=10)
+b1=tk.Button(master=f10, text="Every other month", height=10)
 b1.grid(row=0, column=0, sticky="nsew")
 
-b2=tk.Button(master=f1, text="Stir Fry")
+b2=tk.Button(master=f10, text="Every single day")
 b2.grid(row=0, column=1, sticky="nsew")
 
-b3=tk.Button(master=f1, text="Chicken and Rice", height=10)
+b3=tk.Button(master=f10, text="When my friends go", height=10)
 b3.grid(row=1, column=0, sticky="nsew")
 
-b4=tk.Button(master=f1, text="Cereal and Ice Cream")
+b4=tk.Button(master=f10, text="Never")
 b4.grid(row=1, column=1, sticky="nsew")
 
 
 
-label1=tk.Label(master=window,text="What do you do in your freetime?", width=50, height=10)
+label1=tk.Label(master=content_frame,text="Are you working at a summer camp or getting an internship this summer?", width=50, height=10)
 label1.pack()
 
-f1=tk.Frame(master=window)
-f1.pack(fill=tk.BOTH, expand=True)
+f11=tk.Frame(master=content_frame)
+f11.pack(fill=tk.BOTH, expand=True)
 
-f1.rowconfigure([0,1], minsize=20, weight=1)
-f1.columnconfigure([0,1], minsize=20, weight=1)
+f11.rowconfigure([0,1], minsize=20, weight=1)
+f11.columnconfigure([0, 1], minsize=20, weight=1)
 
-b1=tk.Button(master=f1, text="Read spiritual boooks outside", height=10)
+b1=tk.Button(master=f11, text="Summer camp", height=10)
 b1.grid(row=0, column=0, sticky="nsew")
 
-b2=tk.Button(master=f1, text="Go on long walks with your Frannie significant other")
+b2=tk.Button(master=f11, text="Internship/working")
 b2.grid(row=0, column=1, sticky="nsew")
 
-b3=tk.Button(master=f1, text="Always studying", height=10)
+b3=tk.Button(master=f11, text="Mission Trip!!", height=10)
 b3.grid(row=1, column=0, sticky="nsew")
 
-b4=tk.Button(master=f1, text="I'm a JC rat")
+b4=tk.Button(master=f11, text="I do not know yet")
 b4.grid(row=1, column=1, sticky="nsew")
-
-
-
-label1=tk.Label(master=window,text="How often do you go to Latin Mass?", width=50, height=10)
-label1.pack()
-
-f1=tk.Frame(master=window)
-f1.pack(fill=tk.BOTH, expand=True)
-
-f1.rowconfigure([0,1], minsize=20, weight=1)
-f1.columnconfigure([0,1], minsize=20, weight=1)
-
-b1=tk.Button(master=f1, text="Every other month", height=10)
-b1.grid(row=0, column=0, sticky="nsew")
-
-b2=tk.Button(master=f1, text="Every single day")
-b2.grid(row=0, column=1, sticky="nsew")
-
-b3=tk.Button(master=f1, text="When my friends go", height=10)
-b3.grid(row=1, column=0, sticky="nsew")
-
-b4=tk.Button(master=f1, text="Never")
-b4.grid(row=1, column=1, sticky="nsew")
-
-
-
-label1=tk.Label(master=window,text="Are you working at a summer camp or getting an internship this summer?", width=50, height=10)
-label1.pack()
-
-f1=tk.Frame(master=window)
-f1.pack(fill=tk.BOTH, expand=True)
-
-f1.rowconfigure([0], minsize=20, weight=1)
-f1.columnconfigure([0, 1], minsize=20, weight=1)
-
-b1=tk.Button(master=f1, text="Summer camp", height=10)
-b1.grid(row=0, column=0, sticky="nsew")
-
-b2=tk.Button(master=f1, text="Internship/working")
-b2.grid(row=0, column=1, sticky="nsew")
-
-
-
-
-label1=tk.Label(master=window,text="Matt Fradd or Jordan Peterson?", width=50, height=10)
-label1.pack()
-
-f1=tk.Frame(master=window)
-f1.pack(fill=tk.BOTH, expand=True)
-
-f1.rowconfigure([0], minsize=20, weight=1)
-f1.columnconfigure([0,1], minsize=20, weight=1)
-
-b1=tk.Button(master=f1, text="Matt Fradd", height=10)
-b1.grid(row=0, column=0, sticky="nsew")
-
-b2=tk.Button(master=f1, text="Jordan Peterson")
-b2.grid(row=0, column=1, sticky="nsew")
 
 
 
