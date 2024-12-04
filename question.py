1de7d2d0a210146a366a0f6c57eaed4f818bf6bc
Carolyn Kuebler <ckuebler001@student.franciscan.edu>
2024-12-04 15:34:01 -0500

    works

--- question.py
+++ question.py
@@ -126,24 +126,3 @@ def answer_five():
 def answer_six():
     pass
 
-
-
-# use this 
-## import tkinter as tk
-
-# border_effects = {
-#   "sunken": tk.SUNKEN,
- #   "raised": tk.RAISED,
- #   "groove": tk.GROOVE,
- #   "ridge": tk.RIDGE,
-#}
-
-#window = tk.Tk()
-
-#for relief_name, relief in border_effects.items():
-   # frame = tk.Frame(master=window, relief=relief, borderwidth=5)
-   # frame.pack(side=tk.LEFT)
-   # label = tk.Label(master=frame, text=relief_name)
-   # label.pack()
-
-#window.mainloop()
--- trying_to_organize.py
+++ trying_to_organize.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+
 questions={
     'What sport do you play?' : ['ultimate frisbee', 'volleyball', 'soccer', 'no intramurals for me'],
     'Choose a Household': ['AMDG', 'Fishers', 'DDM', 'Stella'],
@@ -34,44 +35,6 @@ canvas.create_window((0,0), window=content_frame, anchor="nw")
 
 
 
-a=0
-b=0
-c=0
-d=0
-
-
-def twoPDP():
-    global a
-    a+=2
-
-def twoFOP():
-    global b
-    b+=2
-
-def twofloater():
-    global d
-    d+=2
-
-def twoAMDG():
-    global c
-    c+=2
-
-def onePDP():
-    global a
-    a+=1
-
-def oneFOP():
-    global b
-    b+=1
-
-def onefloater():
-    global c
-    c+=1
-
-def oneAMDG():
-    global d
-    d+=1
-
 
 list1=["no intramurals for me", "Fishers", "Scott Hahn", "theocat", "6:30 AM", "stir fry", "Dorm","Go on a walk with \n my Franny significant other", "every single day", "Working at Summer Camp" ]
 list2=["DDM", "ultimate frisbee", "Dr. Reinhard", "psych", "10:00 AM", "cereal and ice crea", "Library", "Library", "Reading outside", "every other month", "Mission Trip!!"]
@@ -81,38 +44,60 @@ list4=["AMDG", "volleyball", "Dr. Knox", "Business", "8:00 PM", "JC", "pizza", "
 list5=["DDM", "Dr. Reinhard", "Library", "Reading outside", "Mission Trip!!", "ultimate frisbee", "psych", "Noon", "chicken and rice", "every other month"]
 list6=["Fishers", "Scott Hahn", "theocat", "6:30 AM", "Dorm", "Go on a walk with \n my Franny significant other", "every single day", "Always studying", "volleyball", "I do not know yet"]
 list7=["no intramurals for me", "Working at Summer Camp", "AMDG", "Dr. Knox", "Business", "8:00 PM", "JC", "pizza", "I am a JC rat", "never" ]
-list8=["stir fry", "cereal and ice creal", "soccer",  "Stella", "Dr. Kuebler", "nursing", "CTT", "Getting an \n internship/working", "10:00 AM", "when my friends go"]
+list8=["stir fry", "cereal and ice cream", "soccer",  "Stella", "Dr. Kuebler", "nursing", "CTT", "Getting an \n internship/working", "10:00 AM", "when my friends go"]
+
+a=0
+b=0
+c=0
+d=0
+
 
 def fun(x):
-    global a, b, c, d
-    if x in list1:
-        twoPDP()
-        
-    elif x in list2:
-        twoFOP()
-        
-    elif x in list3:
-        twofloater()
-        
-    elif x in list4: 
-        twoAMDG()
-        
-def fun2(x):
-    global a, b, c, d
-    if x in list5:
-        onePDP()
-        
-    elif x in list6:
-        oneFOP()
-        
-    elif x in list7:
-        onefloater()
-        
-    elif x in list8:
-        oneAMDG() 
-      
-    print(f'PDP: {a}, FOP: {b}, Floater: {c}, AMDG: {d}')
+    print(x)
 
+    global a, b, c, d
+
+    if x in list1 and x in list6:
+        a+=2
+        b+=1
+    elif x in list1 and x in list7:
+        a+=2
+        c+=1
+    elif x in list1 and x in list8:
+        a+=2
+        d+=1
+   
+    elif x in list2 and x in list5:
+        b+=2
+        a+=1
+    elif x in list2 and x in list7:
+        b+=2
+        c+=1
+    elif x in list2 and x in list8:
+        b+=2
+        d+=1
+   
+        
+    elif x in list3 and x in list5:
+        c+=2
+        a+=1
+    elif x in list3 and x in list6:
+        c+=2
+        b+=1
+    elif x in list3 and x in list8:
+        c+=2
+        d+=1
+
+    elif x in list4 and x in list5: 
+        d+=2
+        a+=1
+    elif x in list4 and x in list6: 
+        d+=2
+        b+=1
+    elif x in list4 and x in list7: 
+        d+=2
+        c+=1
+    
 
 
 for x in questions:
@@ -123,13 +108,53 @@ for x in questions:
     Frame.pack(fill=tk.X, side=tk.TOP)
 
     for e in questions[x]:
-        button=tk.Button(master=Frame, text=e, height=7, width=16, command=lambda x=e: (fun(x), fun2(x)))
+        button=tk.Button(master=Frame, text=e, height=7, width=16, command=lambda x=e: fun(x))
         button.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
 
-submit=tk.Button(master=content_frame, text="Sumbit Answers")
+
+def answer():
+    if a>b and a>c and a>d:
+        print("You are a PDP boy/homeschooled girl")
+        window=tk.Tk()
+        window.title("You are ....")
+        Label=tk.Label(master=window, text="PDP boy/homeschooled girl", height=30, width=30)
+        Label.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        window.mainloop()
+    elif b>a and b>c and b>d:
+        print("You are that one passionate missionary kid/FOP lover")
+        window=tk.Tk()
+        window.title("You are ....")
+        Label=tk.Label(master=window, text="That one passionate missionary kid/FOP lover", height=30, width=30)
+        Label.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        window.mainloop()
+    elif c>a and c>b and c>d:
+        print("You are a social floater")
+        window=tk.Tk()
+        window.title("You are ....")
+        Label=tk.Label(master=window, text="Social floater", height=30, width=30)
+        Label.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        window.mainloop()
+    elif d>a and d>b and d>c:
+        print("AMDG intent/Little Flowers intent")
+        window=tk.Tk()
+        window.title("You are ....")
+        Label=tk.Label(master=window, text="AMDG intent/Little Flowers intent", height=30, width=30)
+        Label.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        window.mainloop()
+    else:
+        print("You are a social floater")
+        window=tk.Tk()
+        window.title("You are ....")
+        Label=tk.Label(master=window, text="Social floater", height=30, width=30)
+        Label.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        window.mainloop()
+ 
+
+submit=tk.Button(master=content_frame, text="Sumbit Answers", command=answer)
 submit.pack()
 
+    
 
 content_frame.update_idletasks()
 canvas.config(scrollregion=canvas.bbox("all"))
